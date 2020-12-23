1e022e56d19f8e770060f5315b2dd6022b6a3f78
André Ponce <andre_epr@outlook.com>
2020-12-23 20:45:33 -0300

    edit done

--- src/components/Formu.js
+++ src/components/Formu.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
 import api from '../services/api' 
 
 import {
@@ -27,7 +27,17 @@ const initialValue = {
 
 export const Formu = ({ modal2, toggle2, id }) => {
 
-  const [values, setValues] = useState(initialValue)
+  const [values, setValues] = useState(id ? null: initialValue)
+  console.log(id)
+
+  useEffect(() => {
+      if(id) {
+        api.get(`/users/${id}`)
+        .then((response) => {
+          setValues(response.data)
+        })
+      }
+  },[id]);
 
 
   function onChange(e) {
@@ -39,7 +49,11 @@ export const Formu = ({ modal2, toggle2, id }) => {
   function onSubmit(e) {
     e.preventDefault();
 
-    api.post('/users', values)
+    const method = id ? 'patch' : 'post'
+    const url = id
+     ? `http://localhost:3333/users/${id}`
+     : `http://localhost:3333/users/`
+    api[method](url, values)
     .then((response) => {
       console.log(response)
     })
@@ -57,27 +71,27 @@ export const Formu = ({ modal2, toggle2, id }) => {
             <Form onSubmit={onSubmit}>
               <FormGroup>
                 <Label for="name">Name</Label>
-                <Input type="text" name="name" id="name" placeholder="Name" onChange={onChange}/>
+                <Input type="text" name="name" id="name" placeholder="Name" onChange={onChange} value={values.name}/>
               </FormGroup>
               <FormGroup>
                 <Label for="LastName">LastName</Label>
-                <Input type="text" name="lastName" id="LastName" placeholder="LastName" onChange={onChange}/>
+                <Input type="text" name="lastName" id="LastName" placeholder="LastName" onChange={onChange} value={values.lastName}/>
               </FormGroup>
               <FormGroup>
                 <Label for="email">Email</Label>
-                <Input type="email" name="email" id="email" placeholder="Email" onChange={onChange}/>
+                <Input type="email" name="email" id="email" placeholder="Email" onChange={onChange} value={values.email}/>
               </FormGroup>
               <FormGroup>
                 <Label for="password">Password</Label>
-                <Input type="password" name="password" id="password" placeholder="Password" onChange={onChange}/>
+                <Input type="password" name="password" id="password" placeholder="Password" onChange={onChange} value={values.password}/>
               </FormGroup>
               <FormGroup>
                 <Label for="Occupation">Occupation</Label>
-                <Input type="text" name="occupation" id="Occupation" placeholder="Occupation" onChange={onChange}/>
+                <Input type="text" name="occupation" id="Occupation" placeholder="Occupation" onChange={onChange} value={values.occupation}/>
               </FormGroup>
               <FormGroup>
                 <Label for="Phone">Phone</Label>
-                <Input type="password" name="phone" id="Phone" placeholder="Phone" onChange={onChange}/>
+                <Input type="password" name="phone" id="Phone" placeholder="Phone" onChange={onChange} value={values.phone}/>
               </FormGroup>
 
               <Button type='submit' color="primary" onClick={() => {toggle2();} } >Submit</Button>{' '}
